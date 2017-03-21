6c0348e9154f2de8d008f88ffb80e669fbdf3ade
zylstraj <colbertlove12@gmail.com>
2017-03-21 16:45:56 -0700

    Search Bar Component Added
    Finished 52. Search Bar component added.

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SearchBar from '../containers/search-bar';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-      Hello
+          <SearchBar />
       </div>
     );
   }
