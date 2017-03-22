6b89424d7fb051d6e0a888cb041a42d3894eb5d0
zylstraj <colbertlove12@gmail.com>
2017-03-22 15:22:50 -0700

    Running into issues connecting API
    Trying to connect API. Running into some sort of problem.

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
   onInputChange(event){
     this.setState({ term: event.target.value });
   }
   onFormSubmit(event) {
     event.preventDefault();
+    // we need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+    // call the term - and then reset the form to blank, empty string
   }
   render() {
     return (
@@ -29,3 +37,8 @@ export default class SearchBar extends React.Component {
     )
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
