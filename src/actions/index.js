8214e65f0ebe562983afa030d47946141286ff5a
zylstraj <colbertlove12@gmail.com>
2017-03-22 16:29:23 -0700

    Trying to connect API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const API_KEY = '0547f7cf3264ad65fb0b9df34fdf9cb1';
-const ROOT_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid={API_KEY}`;
- export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
- export function fetchWeather(city) {
+export function fetchWeather(city) {
    const url = `${ROOT_URL}&q=${city},us`;
+   console.log(url);
    const request = axios.get(url);
    //will return a promise here
    console.log('Request:', request);
