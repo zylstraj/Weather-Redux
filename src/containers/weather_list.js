3a1644777c05f16dd1ae64185b70a25acd9b0245
zylstraj <colbertlove12@gmail.com>
2017-03-29 16:51:00 -0700

    Adding a few changes.
    Thinking about combining with my other project, but that might be
    another time and place. Don't want to get stuck going back, but
    re-working code is/will be my job.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 
 class WeatherList extends React.Component {
   renderWeather(cityData) {
+    console.log(cityData.list[0].main.temperature);
     return (
+      <div key={cityData.city.name}>
       <p>{cityData.city.name}</p>
+      <p>Temperature: {cityData.list[0].main.temp}</p>
+      <p>Description: {cityData.list[0].weather[0].description}</p>
+      </div>
     )
   }
   render() {
